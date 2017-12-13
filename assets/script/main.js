7c46a6b7dfb5973f2e80cca0c413c95f02ec297c
ohsnaplol <emyb@bellsouth.net>
2017-12-13 18:37:16 -0500

    setup firebase

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAU1zkwv38jAxzuRXg7dDAAbv_Q8GqHo7k",
+  authDomain: "dancefloor-20362.firebaseapp.com",
+  databaseURL: "https://dancefloor-20362.firebaseio.com",
+  projectId: "dancefloor-20362",
+  storageBucket: "dancefloor-20362.appspot.com",
+  messagingSenderId: "473688295370"
+};
+firebase.initializeApp(config)
+var database = firebase.database()
+
 $(document).ready(function() {
   $("#danceFloorImg").on("click", function(event) {
     console.log("x: " + event.originalEvent.screenX + " y: " + event.originalEvent.screenY);
