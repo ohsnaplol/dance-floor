5ab48675415ea5bdb43da5ef93d04b92ee0f3263
ohsnaplol <emyb@bellsouth.net>
2017-12-14 20:36:41 -0500

    Revert "added comments"
    This reverts commit f2b9e0f14fee5fc3a88b99c93296c017fe708fb2.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyAU1zkwv38jAxzuRXg7dDAAbv_Q8GqHo7k",
@@ -39,3 +40,46 @@ function fillGifSelect(title, count, offset) {
     }
   });
 }
+=======
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAU1zkwv38jAxzuRXg7dDAAbv_Q8GqHo7k",
+  authDomain: "dancefloor-20362.firebaseapp.com",
+  databaseURL: "https://dancefloor-20362.firebaseio.com",
+  projectId: "dancefloor-20362",
+  storageBucket: "dancefloor-20362.appspot.com",
+  messagingSenderId: "473688295370"
+};
+firebase.initializeApp(config)
+var database = firebase.database()
+
+$(document).ready(function() {
+  fillGifSelect('dance', 20)
+  $("#danceFloorImg").on("click", function(event) {
+    addImage(event.originalEvent.clientX,event.originalEvent.clientY, './assets/img/dancetest.gif');
+  })
+})
+
+function addImage(x, y, url) {
+  var newImage = $('<img>')
+  newImage.attr('class', 'overlays')
+  newImage.attr('src', url)
+  newImage.css('left', x + 'px')
+  newImage.css('top', y + 'px')
+  $('body').append(newImage)
+}
+
+function fillGifSelect(title, count, offset) {
+  var queryURL = `https://api.giphy.com/v1/stickers/search?q=${title}&limit=${count}&offset=${offset}&api_key=dc6zaTOxFJmzC`
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    for (var i = 0; i < count; i++) {
+      $("#gifSelect").append(`
+        <img class='img-thumbnail selectableGif' src='${response.data[i].images.original.url}'>
+      `);
+    }
+  });
+}
+>>>>>>> parent of f2b9e0f... added comments
